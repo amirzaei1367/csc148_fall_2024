0a3695b8a426efc6afc9495ab6746343548b9aef
amirzaei1367 <amirzaei@gmu.edu>
2024-09-09 14:46:24 -0400

    Sep9th

---
+++ week2/week2_example0.py
@@ -0,0 +1,42 @@
+'''
+#shape1
+#method1
+row1 = "****\n"
+row2 = "****\n"
+row3 = "****\n"
+row4 = "****\n"
+shape = row1 + row2 + row3 + row4
+
+
+#method2
+shape = ("*"*4 + "\n")*4
+print(shape)
+
+#shape2
+#method1
+row1 = "****\n"
+row2 = "*  *\n"
+row3 = "*  *\n"
+row4 = "****\n"
+shape = row1 + row2 + row3 + row4
+print(shape)
+
+#shape3
+#method1
+row1 = "****\n"
+row2 = " ***\n"
+row3 = "  **\n"
+row4 = "   *\n"
+shape = row1 + row2 + row3 + row4
+print(shape)
+'''
+#shape3
+#method1
+row1 = "****\n"
+row2 = " ****\n"
+row3 = "  ****\n"
+row4 = "   ****\n"
+shape = row1 + row2 + row3 + row4
+print(shape)
+
+print("hello again!!!")
---
+++ week2/week2_exmaple1.py
@@ -0,0 +1,25 @@
+'''
+a = "hello world"
+print(a)
+print("count", a.count("h"))
+print("upper case", a.upper())
+print("lower case", a.lower())
+
+a_string = "HELLO WORLD"
+b_string = 'oRl'
+print(b_string.lower() in a_string.lower())
+
+a_s = "123456789"
+print(a_s[0::2])
+
+space_index = a.find(" ")
+print(space_index)
+print(a[0:space_index:2] + a[space_index::2])
+print(a[::-1])
+print(a[::-2])
+print(a[-4:-1])
+'''
+
+#a = jasper
+#print(a)
+
